13ba754ac9c935977a3bd29911ac0c9776ac423f
Vlad <vladbabaev11@gmail.com>
2021-11-22 23:23:07 +0300

    mailer update

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -9,7 +9,10 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
     MailerModule.forRoot({
       transport: {
         host: process.env.MAIL_HOST,
+        port: 587,
         secure: false,
+        requireTLS: true,
+
         auth: {
           user: process.env.MAIL_USER,
           pass: process.env.MAIL_PASSWORD,
