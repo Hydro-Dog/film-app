3d6a312cc3bb95338cfe877843b57cf78e7a17d4
Vlad <vladbabaev11@gmail.com>
2021-11-25 00:26:38 +0300

    asdsadddd

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,11 @@
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_NAME
-const DB_URL = process.env.DB_URL
-const ENVIRONMENT = process.env.ENVIRONMENT
-
 module.exports = {
   type: 'postgres',
-  url: DB_URL,
+  host: 'localhost',
+  port: 5432,
+  username: 'vlad',
+  password: 'vlad',
+  database: 'vlad_db',
+  url: '',
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false,
@@ -16,3 +15,21 @@ module.exports = {
     migrationsDir: 'src/migration',
   },
 }
+
+// module.exports =  {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'vlad',
+//   password: 'vlad',
+//   database: 'vlad_db',
+//   url: '',
+//   logging: true,
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+//   synchronize: false,
+//   dropSchema: false,
+//   migrations: ['src/migration/**/*.ts'],
+//   cli: {
+//     migrationsDir: 'src/migration',
+//   },
+// }
