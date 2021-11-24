67621758e03bc35ed8627dd28c68d8b3f32d7c38
Vlad <vladbabaev11@gmail.com>
2021-11-25 00:21:07 +0300

    asdsss

--- ormconfig.js
+++ ormconfig.js
@@ -4,33 +4,15 @@ const DB_NAME = process.env.DB_NAME
 const DB_URL = process.env.DB_URL
 const ENVIRONMENT = process.env.ENVIRONMENT
 
-module.exports =
-  ENVIRONMENT === 'dev'
-    ? {
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_NAME,
-        logging: true,
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        dropSchema: false,
-        migrations: ['src/migration/**/*.ts'],
-        cli: {
-          migrationsDir: 'src/migration',
-        },
-      }
-    : {
-        type: 'postgres',
-        url: DB_URL,
-        logging: true,
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        dropSchema: false,
-        migrations: ['src/migration/**/*.ts'],
-        cli: {
-          migrationsDir: 'src/migration',
-        },
-      }
+module.exports = {
+  type: 'postgres',
+  url: DB_URL,
+  logging: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  dropSchema: false,
+  migrations: ['src/migration/**/*.ts'],
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+}
