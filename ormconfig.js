f5e4cdcd8ef184b5a16ccdd959cc42e43034f0bb
Vlad <vladbabaev11@gmail.com>
2021-11-25 10:04:47 +0300

    config url only

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,5 @@
 module.exports = {
   type: 'postgres',
-  host: 'ec2-3-248-87-6.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  username: 'lliiyodvvgybyo',
-  password: '86bc1416beb0c61c1dc594ab2d2024d34598d9a2ea450467b2ed7cad332b0f11',
-  database: 'd39rf4e6061ks6',
   url: 'postgres://lliiyodvvgybyo:86bc1416beb0c61c1dc594ab2d2024d34598d9a2ea450467b2ed7cad332b0f11@ec2-3-248-87-6.eu-west-1.compute.amazonaws.com:5432/d39rf4e6061ks6',
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
