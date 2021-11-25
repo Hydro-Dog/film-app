387c50acc05bc57d5d99ff565664ffab45f25185
Vlad <vladbabaev11@gmail.com>
2021-11-25 12:15:01 +0300

    db config upd

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,17 @@
 module.exports = {
   type: 'postgres',
-  url: 'postgres://lliiyodvvgybyo:86bc1416beb0c61c1dc594ab2d2024d34598d9a2ea450467b2ed7cad332b0f11@ec2-3-248-87-6.eu-west-1.compute.amazonaws.com:5432/d39rf4e6061ks6',
+  url: 'postgres://lliiyodvvgybyo:86bc1416beb0c61c1dc594ab2d2024d34598d9a2ea450467b2ed7cad332b0f11@ec2-3-248-87-6.eu-west-1.compute.amazonaws.com:5432/d39rf4e6061ks6?sslmode=no-verify',
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false,
   dropSchema: false,
-  migrations: ['src/migration/**/*.ts'],
+  migrations: ['dist/migration/**/*.js'],
   cli: {
     migrationsDir: 'src/migration',
   },
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
 }
 
-// module.exports =  {
+// module.exports = {
 //   type: 'postgres',
 //   host: 'localhost',
 //   port: 5432,
@@ -28,7 +23,7 @@ module.exports = {
 //   entities: ['dist/**/*.entity{.ts,.js}'],
 //   synchronize: false,
 //   dropSchema: false,
-//   migrations: ['src/migration/**/*.ts'],
+//   migrations: ['dist/migration/**/*.js'],
 //   cli: {
 //     migrationsDir: 'src/migration',
 //   },
